54ff7d6601d1459968a2777a06c40890e18a4142
Nicolas M <nicolaspok@msn.com>
2024-02-01 16:46:31 +0100

    chore: fix build (#8)
    * feat: add package command
    
    * chore: fix build
    
    ---------
    
    Co-authored-by: Nicolas Moreau <nicolas.moreau76@gmail.com>

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,8 +3,10 @@ import * as esbuild from 'esbuild'
 await esbuild.build({
     entryPoints: ['./src/index.ts'],
     bundle: true,
-    outdir: 'dist/nodejs',
+    outdir: 'dist/nodejs/customization',
     minify: true,
     treeShaking: true,
     logLevel: 'debug',
+    target: 'node18',
+    platform: 'node'
 })
