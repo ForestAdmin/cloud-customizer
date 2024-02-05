bb1a3d0e677f5127fe1a1d7a163830da711f8fdd
alban bertolini <albanbertolini@MacBook-Pro-de-alban.local>
2024-02-05 12:43:20 +0100

    fix: bump and fix command

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import type { Agent } from '@forestadmin/cloud-toolkit';
 
 import { Schema } from '../typings';
 
+/*
+You can find the documentation of the agent here: https://docs.forestadmin.com/developer-guide-agents-nodejs/.
+The documentation will help you to understand how to use the agent and how to customize your project.
+*/
 export default function customizeAgent(agent: Agent<Schema>) {
-  void agent;
+  agent.customizeCollection('users', (collection) => {
+    collection.addField('HelloWorld', {
+      columnType: 'String',
+      defaultValue: 'Hello World',
+      getValues(records) {
+        return records.map(() => 'Hello World');
+      }
+    })
+  });
 }
