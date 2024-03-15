1e4a514f91d46021da65e1365518d6314744c98b
scra <albanbertolini@gmail.com>
2024-03-15 11:43:05 +0100

    fix: avoid to throws an error on the build by adding an exported function on the index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-/* will be replaced when bootstrapping the project */
+import type { Agent } from "@forestadmin/forest-cloud";
+
+import { Schema } from "../typings";
+
+export default function customizeAgent(agent: Agent<Schema>) {
+
+}
